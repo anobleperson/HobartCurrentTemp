c7160a0f410864041a98b046cc80ed383e38e8a0
Ashley Noble <ash@Ashleys-MacBook-Pro.local>
2016-03-10 22:08:01 +1100

    improved output with return

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
         if (!error && response.statusCode == 200) {
             var bodyObj = JSON.parse(body);
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end('{"CurrentTemperature" : ' + bodyObj['observations']['data'][0]['air_temp'] + '}');
+            res.end('{"CurrentTemperature" : ' + bodyObj['observations']['data'][0]['air_temp'] + '}\n');
         }
         else {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
